c72ac0fefb1ae82bc095891b925670ef1259ab29
KeemL <hlingo3@gatech.edu>
2025-04-05 13:35:01 -0400

    Add base backend files

---
+++ Backend/Backend-MEEPS/src/test/java/com/example/MEEPS/MeepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MEEPS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
