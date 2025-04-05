cc1cc0bdeb8e03d4a88af5bd1bac6dff86de696a
rafael <rgeraldo3@gatech.edu>
2025-04-05 15:10:46 -0400

    Get form information

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,8 +6,15 @@ export default function Home() {
       <div className='w-screen h-screen flex justify-center items-center bg-linear-to-br from-cyan-500 to-blue-500'>
         <form className='flex flex-col gap-6 rounded-xl p-6 shadow-lg bg-white h-fit w-lg' action={async (formData) => {
           'use server'
-          console.log(formData.get('firstName'));
-          console.log(formData.get('lastName'));
+          const firstName = formData.get('firstName');
+          const lastName = formData.get('lastName');
+          const birthDate = formData.get('birthDate');
+          const checkedConditions = formData.getAll('condition');
+
+          console.log(firstName)
+          console.log(lastName)
+          console.log(birthDate)
+          console.log(checkedConditions);
         }}>
           <h1 className='text-2xl font-bold'>Patient Information</h1>
 
@@ -22,8 +29,8 @@ export default function Home() {
 
           <FormLabel component="legend">Relevant Conditions</FormLabel>
           <FormGroup>
-            <FormControlLabel control={<Checkbox name='condition' />} label="Pollen Allergy" />
-            <FormControlLabel control={<Checkbox name='condition' />}label="Pregnancy" />
+            <FormControlLabel control={<Checkbox value="Pollen Allergy" name='condition' />} label="Pollen Allergy" />
+            <FormControlLabel control={<Checkbox value="Pregnancy" name='condition' />} label="Pregnancy" />
             <FormControlLabel control={<Checkbox name='condition' />} label="Cardiovascular disease" />
             <FormControlLabel control={<Checkbox name='condition' />} label="Asthma" />
           </FormGroup>
